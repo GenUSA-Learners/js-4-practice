2a87fef1037e9f516f02cad37c7d23a379c5929d
kathryn wilkinson <kathrynxwilk@gmail.com>
2022-07-11 22:32:36 -0400

    Delete script.js

--- 04-dependant-promises/solved/script.js
+++
@@ -1,8 +0,0 @@
-console.log('Your JS file is linked!');
-
-// Write your code below
-let firstPromise = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve("First Promise")
-	}, 3000);
-})
